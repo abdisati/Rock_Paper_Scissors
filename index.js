d1e242d68d118cc211feb71e603579a2c07fe94c
Abdi Deresa <Abdishegute@gmail.com>
2024-08-15 21:11:11 -0700

    added js choices and display

---
+++ index.js
@@ -0,0 +1,5 @@
+//ROCK PAPER SCISSORS
+
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
