88035af133e3fdf0b79e0a7b0589fdbfd63961f8
Abdi Deresa <Abdishegute@gmail.com>
2024-08-15 21:34:14 -0700

    created a playgame function

--- index.js
+++ index.js
@@ -3,3 +3,27 @@
 const choices = ["rock", "paper", "scissors"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+
+function playgame(playerChoice) {
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
+  let result = "";
+  if (playerChoice === computerChoice) {
+    result = "IT'S A TIE!";
+  } else {
+    switch (playerChoice) {
+      case "rock":
+        result = computerChoice === "scissors" ? "You Win!" : "You Lose!";
+        break;
+      case "paper":
+        result = computerChoice === "rock" ? "You Win!" : "You Lose!";
+        break;
+      case "scissors":
+        result = computerChoice === "paper" ? "You Win!" : "You Lose!";
+        break;
+    }
+  }
+  playerDisplay.textContent = `PlAYER: ${playerChoice}`;
+  computerDisplay.textContent = `Computer: ${computerChoice}`;
+  resultDisplay.textContent = result;
+}
